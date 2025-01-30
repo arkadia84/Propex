ea0e41ab88a717817ccf086eba13f66e91c0854b
compub2024 <compubforus2024@outlook.com>
2025-01-30 21:07:09 +0700

    proex configure

---
+++ src/config.js
@@ -0,0 +1,25 @@
+export const TWITTER_LINK = "";
+export const INSTA_LINK = "";
+
+export const MINT_APP_LINK = "https://mint.propex.app";
+
+export const MINT_APP_HOSTNAME = "mint.propex.app";
+export const PROPEX_LANDING_HOSTNAME = "www.propex.app";
+
+export const getPropexContract = (chainId) => {
+  switch (parseInt(chainId)) {
+    case 1135:
+      return "0xD5c17532f49a4E12C9912EbbD9BE64973F78AfE4";
+    default:
+      return "0xD5c17532f49a4E12C9912EbbD9BE64973F78AfE4";
+  }
+};
+
+export const getRaribleChainName = (chainId) => {
+  switch (parseInt(chainId)) {
+    case 1135:
+      return "lisk";
+    default:
+      return "lisk";
+  }
+};
